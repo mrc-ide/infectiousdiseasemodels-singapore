928229fbabe4c41050464d057180614c324a74b0
Lilith Whittles <lwhittles@users.noreply.github.com>
2022-09-29 11:15:49 +0100

    Rename N0 -> N_init

--- files/introduction/exponential.R
+++ files/introduction/exponential.R
@@ -2,11 +2,11 @@
 deriv(N) <- a * N
 
 # initial conditions of the variables
-initial(N) <- N0
+initial(N) <- N_init
 
 # parameter values
-N0 <- user(1)             # total population size
+N_init <- user(1)             # total population size
 a <- user(0.5)            # growth rate
 
 # additional output
-output(N_analytical) <- N0*exp(a*t)
+output(N_analytical) <- N_init * exp(a * t)
