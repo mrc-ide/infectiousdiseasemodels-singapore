03bb140f4295701c3715ffed26e566ae4aee2819
Lilith Whittles <lwhittles@users.noreply.github.com>
2022-09-29 11:11:22 +0100

    Rename N0 -> N_init, I0 => I_init add more comments

--- files/introduction/seirs.R
+++ files/introduction/seirs.R
@@ -5,23 +5,23 @@ deriv(I) <- gamma * E - (b + sigma + alpha) * I
 deriv(R) <- sigma * I - (b + delta) * R
 
 # initial conditions of the variables
-initial(S) <- N0 - I0
+initial(S) <- N_init - I_init
 initial(E) <- 0
-initial(I) <- I0
+initial(I) <- I_init
 initial(R) <- 0
 
 N <- S + E + I + R
 output(pop) <- N
 
 # parameter values
-N0 <- user(1e7)           # total population size
-I0 <- user(1)             # num infectious cases at start of epidemic
-sigma <- user(1)          # recovery rate (1/mean duration infectiousness)
-gamma <- user(1)
-alpha <- user(0)
+N_init <- user(1e7)       # initial population size
+I_init <- user(1)         # num infectious cases at start of epidemic
+gamma <- user(1)          # 1 / duration latent = 1 week
+sigma <- user(1)          # recovery rate (1/mean duration infectiousness = 1 week)
+alpha <- user(0)          # rate of death from infection
 delta <- user(0)          # waning antibody rate
-b <- user(2.6e-4)         # death rate (average life expectancy of 1 year or 52 weeks)
+b <- user(2.6e-4)         # death rate, average life expectancy of 75y i.e 1/(75*52)
 R0 <- user(5)
 
-Births <- b * N0          # number of births (for a constant population size)
+Births <- b * N_init      # number of births (for a constant population size)
 beta <- R0 * ((b + gamma) / gamma) * (b + alpha + sigma)
