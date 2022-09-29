0e828ff0683bf1633b9b7fe18331f02a5645b471
Lilith Whittles <lwhittles@users.noreply.github.com>
2022-09-29 11:14:05 +0100

    Rename I0 -> I_init Rename nu -> sigma (recovery rate)

--- stochasticity/example3/sis.R
+++ stochasticity/example3/sis.R
@@ -1,29 +1,29 @@
 beta <- user(0.5) # contact rate
-nu <- user(0.3) # recovery
+sigma <- user(0.3) # recovery
 N <- user(1000)  # total population.
 
-I0_at_steady_state <- user(1)
+I_init_at_steady_state <- user(1)
 
-I0 <- user(1)
+I_init <- user(1)
 
 ## Steady-state prevelance
-I_star <- if (beta > nu) N * (beta - nu) / beta else 0
+I_star <- if (beta > sigma) N * (beta - sigma) / beta else 0
 
 dt <- 0.01
 time <- step * dt
 
 ## Deterministic solution
-I_init <- if (I0_at_steady_state > 0) I_star else I0
-output(I_det) <- I_star / (1 + (I_star / I_init - 1) * exp(-(beta - nu) * time))
+I_init <- if (I_init_at_steady_state > 0) I_star else I_init
+output(I_det) <- I_star / (1 + (I_star / I_init - 1) * exp(-(beta - sigma) * time))
 
 ## Stochastic solution
-initial(I) <- if (I0_at_steady_state > 0) round(I_star) else I0
+initial(I) <- if (I_init_at_steady_state > 0) round(I_star) else I_init
 
 FOI <- beta * I / N
 S <- N - I
 
 n_infections <- rbinom(S, FOI * dt)
-n_recoveries <- rbinom(I, nu * dt)
+n_recoveries <- rbinom(I, sigma * dt)
 
 update(I) <- I + n_infections - n_recoveries
 output(S) <- TRUE
